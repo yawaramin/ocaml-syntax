4704724b5138e85f33e45ec9ff73fd3291eadccf
Yawar Amin <yawar.amin@gmail.com>
2024-01-06 01:58:27 -0500

    New approach using RR - Railroad Diagram Generator

--- module-ident.js
+++
@@ -1,9 +0,0 @@
-import rr from "./railroad.js";
-import { lowercaseLetter, numberLiteral, uppercaseLetter } from "./defs.js";
-
-rr.Diagram(
-  rr.Sequence(
-    uppercaseLetter,
-    rr.OneOrMore(rr.Choice(0, "_", uppercaseLetter, lowercaseLetter, numberLiteral)),
-  )
-).addTo(document.getElementById("module-ident-diagram"));
